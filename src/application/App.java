c98d4765f68a7c3932dfd0081552811edc7a96d3
dimoDani <danidiazmoka@gmail.com>
2023-11-01 20:20:43 +0100

    Pool and main, first version

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,50 @@
 package application;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ResourceBundle;
+import thread.WorkerThread;
+
 /**
  *
- * @author Janam
+ * @author Dani
  */
 public class App {
-    
+  
+    private int contador = 0;
+    private static final int PORT = (Integer.parseInt(ResourceBundle.getBundle("config.config").getString("PORT"))) ;
+    private static final int MAXUSERS = (Integer.parseInt(ResourceBundle.getBundle("config.config").getString("MAXUSERS"))) ;
+
+    public void iniciar() {
+          while(true){
+        ServerSocket servidor = null;
+        Socket cliente = null;
+        try {
+            servidor = new ServerSocket(PORT);
+            System.out.println("Waiting for the client to connect");
+            cliente = servidor.accept();
+            System.out.println("Client connected successfully");
+            
+            if(contador < MAXUSERS){
+                //Añadir aqui lo que reciba el Worker
+                contador++;
+                WorkerThread worker = new WorkerThread() {};
+                worker.run();
+            } else {
+            //Lo que haria el programa si se excede el numero maximo de conexiones
+            }
+            
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    }
+    public static void main(String[] args) {
+        App s1 = new App();
+        s1.iniciar();
+}
 }
--- src/pool/Pool.java
+++ src/pool/Pool.java
@@ -7,8 +7,44 @@ package pool;
 
 /**
  *
- * @author Janam
+ * @author Dani
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.Stack;
+
 public class Pool {
     
+    private static final String URL = ResourceBundle.getBundle("config.config").getString("URL");
+    private static final String USER = ResourceBundle.getBundle("config.config").getString("USER");
+    private static final String PASSWORD = ResourceBundle.getBundle("config.config").getString("PASSWORD");
+    private static final int MAXUSERS = (Integer.parseInt(ResourceBundle.getBundle("config.config").getString("MAXUSERS"))) ;
+
+    private static Stack<Connection> connections = new Stack<>();
+
+public static Connection getConnection() throws SQLException {
+        if (connections.size() < MAXUSERS) {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connections.push(connection); 
+        } else {
+            throw new SQLException("No hay conexiones disponibles en el pool.");
+        }
+        return connections.pop();
+    }
+
+    public static void returnConnection(Connection connection) throws SQLException {
+        if (connection != null) {
+            connection.close();
+            connections.push(connection);
+        }
+    }
+
+    public static void closeAllConnections() throws SQLException {
+        while (!connections.isEmpty()) {
+            Connection connection = connections.pop();
+            connection.close();
+        }
+    }
 }
