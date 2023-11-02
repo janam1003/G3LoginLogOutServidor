57bc02e31c73a5f4401191d2739768e4750c91b3
janam1003 <lucasjanamsmile@gmail.com>
2023-11-02 10:13:44 +0100

    serverFactory cleaned

--- src/factory/ServerFactory.java
+++ src/factory/ServerFactory.java
@@ -1,38 +1,11 @@
 package factory;
 
-import Classes.SigninSignup;
-import dao.ServerImplementation;
-
 /**
  *
  * @author Janam
  */
 public class ServerFactory {
 
-    // Static variable to store the SigninSignup instance.
-    private static SigninSignup data;
 
-    /**
-     * Get an instance of the SigninSignup interface. If an instance does not
-     * exist, it is created.
-     *
-     * @return An instance of the SigninSignup interface.
-     */
-    public static SigninSignup getSigninSignup() {
-
-        // Check if the data instance is not previously loaded.
-        if (data == null) {
-
-            /**
-             * If not loaded, create a new instance of SigninSignup using the
-             * ServerImplementation class.
-             */
-            data = (SigninSignup) new ServerImplementation();
-
-        }
-
-        // Return the SigninSignup instance.
-        return data;
-    }
 
 }
